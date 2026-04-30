f27742415f8e864984962ddc64f8062cafb9b4f2
naomi tewolde <ntewolde1@my.yearupunited.org>
2026-04-30 17:47:47 -0400

    Sales Analysis SQL Queries

--- Tewolde_Sales_Analysis.sql
+++ Tewolde_Sales_Analysis.sql
@@ -46,53 +46,54 @@ This gives a clear view of all the calculated monthly revenue.*/
 each month and I created alias's to the Sales_Date from the 
 store_sales(ss) table and transaction_date columns.*/ 
 
+/* We have to pull the Store_Sales (alias being ss) table, which contains all 
+the information about individual sales transactions, and use JOIN, which connects 
+each sale to location(state), to add Store_Locations(alias sl), which contains 
+information about state, to combined the matching column "StoreID". The WHERE filters
+(sl.state) 'Maryland' to exclude all non-Maryland sales. I have to use the subquery 
+alias (all_sales). This helps make sure filtering happens before aggregation
+Then using the GROUP BY function so that I can set the columns names as one row 
+being Year and one Row being month and ORDER BY function to make the chart aligned 
+and clear with support from the GROUP BY function. */
+
 /*=======================================================================*/ 
+
 SELECT 
-	YEAR(Sale_Date) AS year, -- creates year column from the sale_date 
-	MONTH(Sale_Date) AS month, -- creates month column from the sale_date
+	YEAR(Sale_Date) AS year, -- creates year column from the sale_date (sd alias for subquery)
+	MONTH(Sale_Date) AS month, -- creates month column from the sale_date (sd alias for subquery)
 	SUM(revenue) AS total_revenue -- combining the sum of revenue as total_revnue column name
-    
--- We have to pull the Store_Sales (alias being ss) table, which contains all the information about individual 
--- sales transactions, and use JOIN, which connects each sale to location(state), to add Store_Locations(alias sl), which
--- contains information about state, to combined the matching column "StoreID". The WHERE filters (sl.state) 'Maryland'
--- to exclude all non-Maryland sales. 
--- I have to use the subquery alias (all_sales). This helps make sure filtering happens before aggregation
--- Then using the GROUP BY function so that I can set the columns names as one row being Year and one Row being month 
--- and ORDER BY function to make the chart aligned and clear with support from the GROUP BY function. 
 
 FROM 
 (SELECT 
 ss.Transaction_Date 
-	AS Sale_Date,
+	AS Sale_Date, -- alias 
 ss.Sale_Amount 
-	AS revenue
+	AS revenue -- alias
 FROM store_sales ss -- pulling trans_date and sale_amount from ss table
 JOIN store_locations sl -- joining with store_loc with the storeID column
 	ON ss.Store_ID = sl.StoreId
-WHERE sl.State = 'Maryland') -- filtering to find 
+WHERE sl.State = 'Maryland') -- filtering to find Maryland in store_loc
 
-all_sales
-GROUP BY Year, Month
-ORDER BY Year, Month;
+all_sales -- subquery alias temporary name
+GROUP BY Year, Month -- combining year and month 
+ORDER BY Year, Month; -- creating the column order 
 
 
 -- 3) Provide a comparison of total revenue for the specific sales territory and the region it belong to.  
+/*======================================================================================================*/
 -- ANSWER: LOCATION(STATE): Maryland, TOTAL REVENUE FROM SS: 11451615.09
 -- ANSWER: LOCATION(REGION): Northeast, TOTAL REVENUE FROM SS: 24237526.98
 
+/*======================================================================================================*/
 -- CONTEXT: What this script is doing is separating Marylands total revenue and the regions 
 -- total revenue. We have to aggregate total revenue for a clear, consise view of the comparision between 
 -- both totals. 
+/*======================================================================================================*/
 
--- I used the WITH clause which just creates a tempory named result just for this single
--- query so that the query is easier to read, which I set as md_region. I used the WHERE function
--- so that it could filter through region column FROM the management table for states that are listed
--- as 'Maryland' 
-
-WITH md_region AS (
+WITH md_region AS ( -- Temporary query name 
     SELECT Region
     FROM management
-    WHERE State = 'Maryland'),
+    WHERE State = 'Maryland'), 
    
 all_sales AS
 
@@ -101,32 +102,24 @@ sl.State,
 ss.Sale_Amount AS Revenue
 FROM store_sales ss
 JOIN store_locations sl
-	ON ss.Store_ID = sl.StoreId)
+	ON ss.Store_ID = sl.StoreId) -- Select statement to match store and location from StoreID 
 
-SELECT
+SELECT                        
     Location,
-    SUM(Revenue) AS Total_Revenue
-    
- /* Maryland total revenue: This SELECT statement I made combines the all_sales subquery to 
-filter revenue and location from 'Maryland' using the WHERE function. */
-FROM (SELECT
+    SUM(Revenue) AS Total_Revenue -- connecting location to look for the sum AS total revenue column name 
+
+FROM (SELECT -- finding Maryland 
 'Maryland' AS 
 
 Location,
 	Revenue
 FROM all_sales
-WHERE State = 'Maryland'
+WHERE State = 'Maryland' 
 
     UNION ALL -- combining both queries, without removing duplicates since we're pulling information 
     -- from a specific state and the states from a specific region
 
-/* Northeast region total: In this SELECT statement we have to find the total revenue from 
-the Northeast region by pulling from the all_sales (s) subquery and the management (m) table 
-and combining them from the shared information which is state, and not also forgeting that 
-the all_sales query pulls information from total revenue and location. Also we created md_region 
-to pull all the comparison states that align with the same region as Maryland, which would be Northeast */
-
-SELECT
+SELECT -- finding region
 	m.Region AS Location,
 	s.Revenue
 FROM all_sales s
@@ -146,17 +139,13 @@ the year and month(and we're looking for the sales date) as well as category ite
 column(which this will come from the inner query). 'Count(*)' helps count all the rows that 
 exist in each group so for each row we'll see a transaction amount. AVG is just calculating
  average transaction size(which 'revenue' will be included in the subquery with an alias) */
-
-SELECT YEAR(Sale_Date) AS Year, 
-		MONTH (Sale_Date) AS Month,  
-        Category,  
-        COUNT(*) AS Number_of_Transactions, 
-        AVG(Revenue) AS Avg_Transaction 
-
+ 
+ /*=======================================================================================*/
+ 
 /* I created an inner query pulling all the information from
- store_sales (ss) table transaction dates and I renamed it as 
- sale_date to be more clear. I pulled the column ss.sale_amount 
- which shows the amount from each sale and renamed it as revenue. 
+store_sales (ss) table transaction dates and I renamed it as 
+sale_date to be more clear. I pulled the column ss.sale_amount 
+which shows the amount from each sale and renamed it as revenue. 
 I also used c.category to pull all the category names. Then I joined 
 store_sales and store_location through the same column name which was 
 StoreID so that it can link the store sales and the location of those for
@@ -164,6 +153,14 @@ sales. I also created a script in the inner query JOINING products and store sal
 so that each sale had a link to store sales and product. The last JOIN I did was
 joining the inventory_categories to products using the categoryID so that there 
 can be grouping and comparison */
+
+/*=======================================================================================*/
+
+SELECT YEAR(Sale_Date) AS Year, 
+		MONTH (Sale_Date) AS Month,  
+        Category,  
+        COUNT(*) AS Number_of_Transactions, 
+        AVG(Revenue) AS Avg_Transaction 
 
 FROM (SELECT 
 ss.Transaction_Date 
@@ -180,11 +177,12 @@ FROM store_sales AS ss
 	JOIN inventory_categories AS c 
 		ON p.Categoryid = c.Categoryid
 WHERE sl.State = 'Maryland')
+
 /* This WHERE function ensures that I'm filtering 
 store location and state which would be Maryland. 
 This keeps the sales in from Maryland */
 
-AS all_sales   -- ends inner query
+AS all_sales  -- ends inner query
 GROUP BY YEAR(Sale_Date),
 		MONTH(Sale_Date),
         Category
@@ -192,18 +190,22 @@ GROUP BY YEAR(Sale_Date),
 ORDER BY Year,
 		Month,
         Category;
+
 /* GROUP BY function just shows how it summarizes the data and 
 the ORDER BY function shows the order of the results */
 
 
 -- 5) Can you provide a ranking of in-store sales performance by each store in the sales territory, or a
 -- ranking of online sales performance by state within an online sales territory?
+/*=====================================================================================================*/
 
 /*CONTEXT: I did a WITH function to create a temporary name to exist in this query create 
 UNIONs. the SELECT statement is just defining the columns I want to include. sl.StoreID is 
 pulling each store separately. m.Region shows the management region. I used the
 SUM function AS Store revenue so it can show one total revenue per store.*/
-WITH store_totals AS (
+/*=======================================================================================*/ 
+
+WITH store_totals AS ( -- Temporary name 
     SELECT
         sl.StoreId,
         m.Region,
@@ -213,7 +215,7 @@ WITH store_totals AS (
         ON ss.Store_ID = sl.StoreId
     JOIN management m
         ON sl.State = m.State -- this JOIN shows which region each store belongs to from sl and m 
-    WHERE m.Region = (
+    WHERE m.Region = ( -- filtering m.Region shows the management region
         
 SELECT Region
         FROM management
@@ -221,16 +223,18 @@ SELECT Region
     ) 
     -- The WHERE function (m.region and SELECT region) separates region and Maryland and
 	-- the subquery finds the region that Maryland belongs to
+    
     GROUP BY sl.StoreId, m.Region
 ) 
 /*OUTER select pulls information from my first WITH query */
+
 SELECT
     StoreId,
     Region,
-    Store_Revenue,
+    Store_Revenue, -- the column order 
     RANK() OVER ( -- ranking store by revenue total
-        ORDER BY Store_Revenue DESC
-    ) AS Store_Rank
+        ORDER BY Store_Revenue DESC -- most to least 
+    ) AS Store_Rank -- alias 
 FROM store_totals -- pulls from the Store totals 
 ORDER BY Store_Rank; -- sorts for row order
 
